b447653341a709bdcd5448c094619c5b5d1e8aef
Taif Faris Al-atif <124814202+TaifAlatif@users.noreply.github.com>
2025-07-08 11:36:43 +0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,47 @@
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyShoppingListAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   Navigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    val viewModel: LocationViewModel = viewModel()
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+    
+    NavHost(navController, startDestination = "shoppinglistscreen"){
+        composable("shoppinglistscreen"){
+            ShoppingListApp(
+                locationUtils = locationUtils,
+                viewModel = viewModel,
+                navController = navController,
+                context = context,
+                address = viewModel.address.value.firstOrNull()?.formatted_address ?: "No Address"
+            )
+        }
+
+        dialog("locationscreen"){backstack ->
+            viewModel.location.value?.let {it1 ->
+
+                LocationSelectionScreeen(location = it1, onLocationSelected ={locationdata ->
+                    viewModel.fetchAddress("${locationdata.latitude}, ${locationdata.longitude}")
+                    navController.popBackStack()
+                } )
+            }
+            
+        }
+    }
+}
