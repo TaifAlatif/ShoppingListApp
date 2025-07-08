5aa6f465733c1207e70f7f8f2db107802e6f654d
Taif Faris Al-atif <124814202+TaifAlatif@users.noreply.github.com>
2025-07-08 11:40:05 +0300

    Create LocationData.kt

---
+++ LocationData.kt
@@ -0,0 +1,13 @@
+data class LocationData(
+    val latitude: Double,
+    val longitude: Double
+)
+
+data class GeocodingResponse(
+    val results: List<GeoCodingResult>,
+    val status: String
+)
+
+data class GeoCodingResult(
+    val formatted_address:String
+)
